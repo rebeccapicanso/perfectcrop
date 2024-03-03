b69bcb4c926b2f409d364b5b06a80a90583a4488
Rebecca Picanso <76741837+rebeccapicanso@users.noreply.github.com>
2024-03-02 21:47:55 -0500

    readability, format, multiple commands

--- perfectcrop/cli.py
+++ perfectcrop/cli.py
@@ -1,40 +1,90 @@
 import argparse
 from . import grid, perfect_crop, shots, video_standardize
 
-#   -p   [video or directory]    | crop a video or directory of clips to select label (runs perfect_crop.py)
-
-#   -s   [video]                 | create .json file identifying shots (scene changes) in a long form video. shots.py will then splice the video into individual clips (or scenes).
-  
-#   -vs  [video or directory]    | standardizes video length, codecs, etc. with ffmpeg bash. needed for concatenation!
-  
-#   -g   [video or directory]    | creates a dynamic, randomized moving video grid with ffmpeg bash commands. this is a legacy file from Court Laureate, not necessary for the project itself :)
-
-#   -l   [video or directory]    | for a select object, identifies what shots that label appears, and then records the average location of that object.
-
 def main():
-    parser = argparse.ArgumentParser(description="Perfect Crop is a tool to crop videos to a specific object in the video.")
-    parser.add_argument("-p", "--perfect_crop", help="crop a video or directory of clips to select label", nargs=2, metavar=("label", "video"))
-    parser.add_argument("-s", "--shots", help="create .json file identifying shots (scene changes) in a long form video. shots.py will then splice the video into individual clips (or scenes).", metavar="video")
-    parser.add_argument("-vs", "--video_standardize", help="standardizes video length, codecs, etc. with ffmpeg bash. needed for concatenation!", metavar="video")
-    parser.add_argument("-g", "--grid", help="creates a dynamic, randomized moving video grid with ffmpeg bash commands. this is a legacy file from Court Laureate, not necessary for the project itself :)", metavar="video")
-    parser.add_argument("-l", "--label", help="for a select object, identifies what shots that label appears, and then records the average location of that object.", metavar="video")
-    args = parser.parse_args()
+    """
+    Run the command line version of Perfect Crop
+    """
+    parser = argparse.ArgumentParser(
+        description="Perfect Crop is a tool to crop videos to a specific object in the video."
+    )
+    parser.add_argument(
+        "--input",
+        "-i",
+        help="The input video file or directory of video files to process.",
+        nargs="*",
+        required=True,
+        dest="input")
 
-    if args.perfect_crop:
-        perfect_crop.save_images(args.perfect_crop[1], args.perfect_crop[0])
-        perfect_crop.average_crop(args.perfect_crop[1], perfect_crop.average_center)
-    elif args.shots:
-        shots.shots(args.shots)
-    elif args.video_standardize:
-        video_standardize.standardize(args.video_standardize)
-    elif args.grid:
-        grid.nightmare_grid(args.grid)
-    elif args.label:
-        perfect_crop.save_images(args.label[1], args.label[0])
-        perfect_crop.average_crop(args.label[1], perfect_crop.average_center)
+    parser.add_argument(
+        "-p",
+        "--perfectcrop",
+        dest="perfectcrop",
+        action="append",
+        help="Crop a video or directory of clips to select label (runs perfect_crop.py)"
+        )
+    
+    # label used for object detection
+    parser.add_argument(
+        "-l",
+        "--label",
+        dest="label",
+        action="append",
+        help="Label used for object detection with perfect_crop.py"
+    )
+
+    parser.add_argument(
+        "-s", 
+        "--shots",
+        dest="shots",
+        action="append",
+        help="Create .json file identifying shots (scene changes) in a long form video. shots.py will then splice the video into individual clips (or scenes)."
+        )
+    parser.add_argument(
+        "-vs", 
+        "--standardize",
+        dest="standardize",
+        action="append",
+        help="Standardizes video length, codecs, etc. with ffmpeg bash. needed for concatenation!")
+    
+    parser.add_argument(
+        "-g", 
+        "--grid",
+        dest="grid",
+        action="append",
+        help="Creates a dynamic, randomized moving video grid with ffmpeg bash commands. this is a legacy file from Court Laureate, not necessary for the project itself :)")
+
+    parser.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        help="Show this help message and exit."
+    )
+
+    args = parser.parse_args()
+    print(args)
+
+    if args.perfectcrop:
+        perfect_crop.save_images(args.input, args.label)
+        perfect_crop.average_center(args.input)
+
+    if args.shots:
+        shots(args.input)
+
+    if args.standardize:
+        video_standardize(args.input)
+    
+    if args.grid:
+        print("please note that the input must be a directory of clips, not a single file.")
+        grid(args.input)
+
+    if args.help:
+        print("Perfect Crop is a tool to crop videos to a specific object in the video.\n" +
+              "Usage: perfectcrop -i [INPUT] [OPTIONS]" )
+    
     else:
-        print("Please use the correct flags to run Perfect Crop.")
-        print("-p flag for perfect crop, followed by label flag, followed by video file")
-        print("-s flag for shots, followed by video file")
-        print("-vs flag for video standardize, followed by video file")
-        print("-g flag for nightmare grid, followed by video file")
+        print("No tasks specified. Please run -h or --help for more information.")
+
+
+if __name__ == "__main__":
+    main()
