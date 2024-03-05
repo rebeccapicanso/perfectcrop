150e65e0df3f39d4a9ac596a7a9a9e3230aaa12a
Rebecca Picanso <76741837+rebeccapicanso@users.noreply.github.com>
2024-03-05 12:14:37 -0500

    Update perfect_crop.py, explained logic
    explained logic!

--- perfectcrop/perfect_crop.py
+++ perfectcrop/perfect_crop.py
@@ -26,6 +26,8 @@ def save_images(input, output, search=LABEL):
     # set input as videofile
     videofile = input
     clip = VideoFileClip(videofile)
+    
+    # initializing..
     index = 0
     frame_no = 0
 
@@ -33,19 +35,29 @@ def save_images(input, output, search=LABEL):
     if not os.path.exists("output"):
         os.makedirs("output")
 
+    
     for frame in clip.iter_frames():
         frame_no += 1
         if frame_no % SKIP != 0:
             continue
 
         # print("Detecting objects in frame", frame_no)
-
+        
+        # calling Pillow's Image function
         image = Image.fromarray(frame)
 
+        # then tossing that image into the pipeline
+        # i.e. is it there or not?
         results = pipe(image)
 
         for r in results:
             # print(r)
+            
+            # if the labeled object is in frame, yay
+            # YOLO annotative obj. detect algos draw the bounding box (the square it throws around something)
+            # based on max & min values. here, i'm just looking at the cross points.
+            # a bounding box is an ephemeral thing. it's not recorded, its an internal throw away process.
+            
             if r["label"] == search:
                 box = r["box"]
 
@@ -61,12 +73,18 @@ def save_images(input, output, search=LABEL):
                     # this is giving a harmless global error... not sure why
                     img_read= cv2.imread(r'image.png')
                     # hide the global loadsave error
+                    # ^^ didnt get to this, check back for v2
                     
-                    
+                    # as the box isn't a real recorded thing,
+                    # i'm using cv2's rectangle function, which is mostly used
+                    # to determine pixel color
                     cv2.rectangle(img_read,(xmin,ymin),(xmax,ymax),(0,0,255),3)
+                    
+                    # calculating cross points
                     center_x = int((xmin+xmax)//2)
                     center_y = int((ymin+ymax)//2)
-
+                    
+                    # center pixel to an array
                     center = [center_x,center_y]
                     average_center.append(center)
                     # print(center)
